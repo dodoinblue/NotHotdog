f3514bb6dae2b9acf51fa03aa0072b10b0ea7279
Charles Liu <dodoinblue@gmail.com>
2017-12-31 10:43:28 -0800

    Initial Commit

---
+++ NotHotdog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotHotdog
+//
+//  Created by charles.liu on 2017-12-31.
+//  Copyright © 2017 Charles Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
