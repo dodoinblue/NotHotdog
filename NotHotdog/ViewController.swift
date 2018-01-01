a6a15c7ee06f845ac51f10c0959a2c5160c4d4eb
Charles Liu <dodoinblue@gmail.com>
2017-12-31 16:34:50 -0800

    async detect

--- NotHotdog/ViewController.swift
+++ NotHotdog/ViewController.swift
@@ -9,14 +9,18 @@
 import UIKit
 import CoreML
 import Vision
+import NVActivityIndicatorView
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, NVActivityIndicatorViewable {
     @IBOutlet weak var imageView: UIImageView!
     
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("UIScreen.main.bounds: \(UIScreen.main.bounds)")
+        let _ = NVActivityIndicatorView(frame: UIScreen.main.bounds,
+                                                            type: NVActivityIndicatorType(rawValue: 14)!)
         
         imagePicker.delegate = self
         imagePicker.sourceType = .camera
@@ -24,24 +28,33 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("imagePickerController didFinishPickingMediaWithInfo")
+        let size = CGSize(width: 30, height: 30)
+        startAnimating(size, message: "Processing...", type: NVActivityIndicatorType(rawValue: 14)!)
+        imagePicker.dismiss(animated: true, completion: nil)
+        
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = pickedImage
             
             guard let ciimage = CIImage(image: pickedImage) else {
                 fatalError("cannot convert to CIImage")
             }
-            
-            detect(image: ciimage)
+
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+                print("async task")
+                self.detect(image: ciimage)
+                self.stopAnimating()
+            }
+            print("after async task declaration")
         }
-        
-        imagePicker.dismiss(animated: true, completion: nil)
     }
     
     func detect(image: CIImage) {
+        print("detect")
         guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
             fatalError("cannot load model")
         }
-        
+
         let request = VNCoreMLRequest(model: model) {(request, error) in
             guard let results = request.results as? [VNClassificationObservation] else {
                 fatalError("wrong classification observation format")
@@ -65,7 +78,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 
     @IBAction func cameraButtonPressed(_ sender: Any) {
-        present(imagePicker, animated: true, completion: nil)
+        print("camerabutton pressed")
+        present(imagePicker, animated: true) {
+            print("imagepicker completion")
+        }
     }
     
 }
